351f99c9d9781c1e6a17ddcb5a3f06bf51ac55a8
meniNaft <mnaft404@gmail.com>
2024-12-24 20:26:52 +0200

    handle error by turn to groq api

--- app/external_api/groq_api.py
+++ app/external_api/groq_api.py
@@ -1,8 +1,7 @@
 import json
 import os
-from tkinter import mainloop
 from typing import Any
-
+from datetime import datetime
 from groq import Groq
 from dotenv import load_dotenv
 
@@ -10,34 +9,50 @@ load_dotenv(verbose=True)
 
 GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 
+expected_schema = {
+    "body": str,
+    "title": str,
+    "lat": float,
+    "lon": float,
+    "date": datetime.date,
+    "category": str,
+}
+
 
 def post_groq_api(article_content: dict) -> Any | None:
     client = Groq(api_key=GROQ_API_KEY)
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
+
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[{
                 "role": "user",
                 "content": (
                     f"{json.dumps(article_content)}\n\n"
                     "This is an article. I want to analyze a few things:\n"
-                    "1. In what country did it happen?\n"
-                    "2. Classify the article into one of the following categories: general news, historical terror attack, or nowadays terror attack.\n\n"
-                    "After analyzing, provide a JSON with the following structure:\n"
+                    "1. Extract the city mentioned in the article.\n"
+                    "2. Provide the latitude and longitude of that city.\n"
+                    "3. Classify the article into one of the following categories: general, history, news.\n\n"
+                    "After analyzing, respond with a JSON that contains the following fields:\n"
                     "{\n"
                     "    \"category\": \"str\",\n"
-                    "    \"country\": \"str\",\n"
                     "    \"city\": \"str\",\n"
-                    "    \"continent\": \"str\",\n"
-                    "    \"country_longitude\": \"int\",\n"
-                    "    \"country_latitude\": \"int\",\n"
+                    "    \"lat\": \"float\",\n"
+                    "    \"lon\": \"float\"\n"
                     "}\n\n"
                     "Respond with the JSON only, without any extra text."
                 ),
-            }
-        ],
-        model="llama3-8b-8192",
-    )
-    try:
-        return json.loads(chat_completion.choices[0].message.content)
-    except:
+            }],
+            model="llama3-8b-8192",
+        )
+
+        response = json.loads(chat_completion.choices[0].message.content)
+        return {
+            "category": response.get("category", ""),
+            "city": response.get("city", ""),
+            "lat": response.get("lat", 0.0),
+            "lon": response.get("lon", 0.0)
+        }
+
+    except Exception as e:
+        print(f"Error processing the response: {e}")
         return None
--- app/services/main_news_handler.py
+++ app/services/main_news_handler.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from app.external_api.groq_api import post_groq_api
-from app.external_api.news_api import fetch_news, get_mock
+from app.external_api.news_api import fetch_news
 from app.services.kafka_service.producer import producer_send_message
 
 load_dotenv(verbose=True)
@@ -15,15 +15,14 @@ def main_processor():
     next_iteration_time = current_time + timedelta(minutes=2)
     counter = 1
     while True:
-        # news = fetch_news(counter)
-        news = get_mock()
+        news = fetch_news(counter)
         for index, elem in enumerate(news):
             print("index: ", index)
             res = post_groq_api(elem)
-            if res and res['category'] != 'general news':
+            if res and res['category'] != 'general':
                 additional_info = {key: elem[key] for key in ["date", "title", "body"] if key in elem}
                 produced_object = {**res, **additional_info}
-                print(produced_object)
+                print(datetime.now(), "produced_object is sent")
                 producer_send_message(NEWS_TOPIC, produced_object, key=str(counter))
         counter += 1
         remaining_time = (next_iteration_time - datetime.now()).total_seconds()
